bf62c198d105330d90c7a8c261595c2a9011a123
supriyo15998 <supriyo15998@gmail.com>
2021-02-19 14:38:12 +0530

    Changes

--- app/src/main/java/me/farazappy/expensetracker/models/Day.java
+++ app/src/main/java/me/farazappy/expensetracker/models/Day.java
@@ -1,6 +1,7 @@
 package me.farazappy.expensetracker.models;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class Day implements Serializable {
     private int id;
@@ -42,5 +43,35 @@ public class Day implements Serializable {
 
     public void setCreatedAt(String createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public int getMonth() {
+        HashMap<String,Integer> monthsMapping = new HashMap<String,Integer>();
+        monthsMapping.put("January",1);
+        monthsMapping.put("February",2);
+        monthsMapping.put("March",3);
+        monthsMapping.put("April",4);
+        monthsMapping.put("May",5);
+        monthsMapping.put("June",6);
+        monthsMapping.put("July",7);
+        monthsMapping.put("August",8);
+        monthsMapping.put("September",9);
+        monthsMapping.put("October",10);
+        monthsMapping.put("November",11);
+        monthsMapping.put("December",12);
+        String str = new StringBuilder(this.getName()).reverse().toString().substring(6);
+        String newStr = "";
+        for(int j=0;j<str.length();j++)
+        {
+            if(str.charAt(j) != ' ')
+            {
+                newStr += String.valueOf(str.charAt(j));
+            }
+            else {
+                break;
+            }
+        }
+        newStr = new StringBuilder(newStr).reverse().toString();
+        return monthsMapping.get(newStr);
     }
 }
